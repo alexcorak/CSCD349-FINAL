dfdd87bcc943289208d074fa96530fe244c088b0
Condos <ncondos@ewu.edu>
2018-11-15 14:46:29 -0800

    room2

--- heroesAndMonsters/Room.java
+++ heroesAndMonsters/Room.java
@@ -2,65 +2,67 @@ package heroesAndMonsters;
 
 public class Room
 {
-	private String[][] room = new String[3][3];
+	private char[] room;
 	private boolean north = true;
 	private boolean south = true;
 	private boolean east = true;
 	private boolean west = true;
+	private char content;
 	
 	
-	public Room(int x, int y, int lengthOfDungeon)
+	public Room(int row, int col, int lengthOfDungeon)
 	{
-		if (x == 0) {
-			this.west = false;
-			room[1][0] = "*";
-		}
-		if (x == lengthOfDungeon - 1) {
-			this.east = false;
-			room[2][1] = "*";
-		}
+		room = new char[11];
+		room[0] = '*';
+		room[2] = '*';
+		room[3] = '\n';
+		room[7] = '\n';
+		room[8] = '*';
 		
-		if (y == 0) {
+		room[10] = '*';
+		room[5] = 'C';
+		
+		if (row == 0) {			 
 			this.north = false;
-			room[0][1] = "*";
+			room[1] = '*';
+			
 		}
-		if (y == lengthOfDungeon - 1) {
+		if (row == lengthOfDungeon - 1) {
 			this.south = false;
-			room[2][1] = "*";
+			room[9] = '*';
 		}
 		
-		room[0][0] = "*";
-		room[2][0] = "*";
-		room[0][2] = "*";
-		room[2][2] = "*";
+		if (col == 0) {
+			this.west = false;
+			room[4] = '*';
+			
+		}
+		if (col == lengthOfDungeon - 1) {
+			this.east = false;
+			room[6] = '*';
+			
+		}
 		
-		room[1][1] = "C";
 		
+		if (north)
+			room[1] = '-';
+		if (south)
+			room[9] = '-';
+		if (west)
+			room[4] = '|';
+		if (east)
+			room[6] = '|';
+			
 		
 			
 	}
 	
-	public void printRoom()
+	
+	public String toString()
 	{
-		if (north)
-			room[0][1] = "-";
-		if (south)
-			room[2][1] = "-";
-		if (west)
-			room[1][0] = "|";
-		if (east)
-			room[1][2] = "|";
 		
-		for (int row = 0; row < room.length; row++)
-		{
-			for (int col = 0; col < room.length; col++)
-			{
-				
-					
-				System.out.print(room[row][col]);
-			}
-			System.out.println();
-		}
+		String str = new String(room);
+		return str;
 	}
 	
 	
